5c7e5e62a937a364bb3d5c9ede46b99537c77f18
STadds <staddi@gmail.com>
2020-05-30 17:58:09 -0400

    get api/workouts, stubb post, put, workouts/range

--- routes/workout-routes.js
+++ routes/workout-routes.js
@@ -1,4 +1,23 @@
 const router = require("express").Router();
 const db = require("../models");
 
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .sort({ _id: -1 })
+    .limit(-1)
+    .then((dbWorkout) => {
+      console.log(dbWorkout);
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/api/workouts", ({ body }, res) => {});
+
+router.put("/api/workouts/:id", (req, res) => {});
+
+router.get("/api/workouts/range", (req, res) => {});
+
 module.exports = router;
