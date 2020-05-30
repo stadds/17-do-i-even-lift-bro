5240878156299335d9dd00ed575ee4c83e0a822d
STadds <staddi@gmail.com>
2020-05-30 19:02:12 -0400

    post Workout put (push exercise onto current

--- routes/workout-routes.js
+++ routes/workout-routes.js
@@ -2,11 +2,12 @@ const router = require("express").Router();
 const db = require("../models");
 
 router.get("/api/workouts", (req, res) => {
+  console.log("get api/workouts");
+
   db.Workout.find({})
     .sort({ _id: -1 })
     .limit(-1)
     .then((dbWorkout) => {
-      console.log(dbWorkout);
       res.json(dbWorkout);
     })
     .catch((err) => {
@@ -14,10 +15,40 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
-router.post("/api/workouts", ({ body }, res) => {});
+router.put("/api/workouts/:id", (req, res) => {
+  console.log("in workout/:id  : " + req.params.id + ",  " + req.body);
 
-router.put("/api/workouts/:id", (req, res) => {});
+  db.Workout.updateOne(
+    {
+      _id: req.params.id,
+    },
+    {
+      $push: { exercises: req.body },
+    }
+  )
+    .then((dbExercise) => {
+      res.json(dbExercise);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
 
-router.get("/api/workouts/range", (req, res) => {});
+router.post("/api/workouts", ({ body }, res) => {
+  console.log("post workouts " + body);
+
+  db.Workout.create(body)
+    .then((dbWorkout) => {
+      console.log("success: " + dbWorkout);
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  console.log("in /api/workouts/range" + req.body);
+});
 
 module.exports = router;
