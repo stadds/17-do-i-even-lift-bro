46235a539b16952c950905fadd591722ac4e281c
STadds <staddi@gmail.com>
2020-05-30 16:03:51 -0400

    setup html routes, let server get them

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 
 
 app.use(require("./routes/workout-routes.js"));
+app.use(require("./routes/html-routes.js"));
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
